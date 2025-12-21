1072212c55d53f14916aca77d4e2a5b8ab972d90
Hoovie105 <sarhan.saif0@gmail.com>
2025-12-20 19:52:42 -0500

    more backend stuff

--- app/database.py
+++ app/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:123@localhost:5432/equipment_db" # Hide your env kids🤐
-)
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
